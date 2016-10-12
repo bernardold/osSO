f6513ef7a39c83a91f230142fd570c383cd11eba
jhonathanviudes <jhonathan.viudes@gmail.com>
2016-10-12 16:47:32 -0300

    teste

--- boot/boot.s
+++ boot/boot.s
@@ -65,6 +65,13 @@ print_welcome2:
 	mov $0xB800, %ax
 	mov %ax, %ds
 	mov $msg_cmd, %si
+	
+	mov $0x0A, %ah //teste
+	mov $0x61, %al
+	mov $0x00, %bh 
+	mov $0x10, %bl 
+	mov $0x03, %cx 
+	int $0x10
 	jmp move
 print_foo:
 	mov $msg_foo, %si
