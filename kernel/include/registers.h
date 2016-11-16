78a4c92b70b8d7bb3dc88750b5419ebb235a5b56
Bernardo Duarte <bernardolageduarte@gmail.com>
2016-11-16 18:56:46 -0200

    adds kernel partialy

---
+++ kernel/include/registers.h
@@ -0,0 +1,8 @@
+#ifndef _REGISTERS_H_
+#define _REGISTERS_H_
+
+int getCS ();
+int getDS ();
+int getSS ();
+
+#endif
---
+++ kernel/include/stddef.h
@@ -0,0 +1,7 @@
+#ifndef _STDDEF_H
+#define _STDDEF_H
+
+#define NULL ((void*)0)
+typedef unsigned size_t;
+
+#endif
